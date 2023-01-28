1935b67531c113a1f71f356a7552bcf2e691e71d
tokkaya79 <tokkaya79@gmail.com>
2023-01-28 14:39:31 +0200

    added getProducts from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import {MatCardModule} from '@angular/material/card';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IProducts } from 'src/app/models/products';
+import { ProductsService } from 'src/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products: IProducts[]
+  productsSubscription: Subscription
+
+
+  constructor(private ProductsService: ProductsService) { }
+
+
+  ngOnInit(): void {
+    this.productsSubscription = this.ProductsService.getProducts().subscribe((data) => { this.products = data})
+  }
+
+
+  ngOnDestroy() {
+    if (this.productsSubscription) this.productsSubscription.unsubscribe()
+  }
 
 }
