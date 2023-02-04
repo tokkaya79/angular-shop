713fcdfe7ded3bd52362f398d0438c2a0037a1c3
tokkaya79 <tokkaya79@gmail.com>
2023-02-04 08:59:43 +0200

    fixed header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundError } from 'rxjs';
 import { ProductResolver } from 'src/services/product.resolver';
-import { BaseComponent } from './components/base/base.component';
+
 import { BasketComponent } from './components/basket/basket.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductsComponent } from './components/products/products.component';
 
 
 const routes: Routes = [
-  {path: '', component: BaseComponent},
-  {path: 'products', component: ProductsComponent},
+
+  {path: '', component: ProductsComponent},
   {path: 'product/:id', component: ProductDetailComponent, resolve: {data: ProductResolver}},
   {path: 'basket', component: BasketComponent},
 
